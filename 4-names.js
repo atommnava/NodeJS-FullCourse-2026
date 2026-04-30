a24aab95518af294e91b95e4430e4d0f30e411f3
atommnava <atommunoznava@gmail.com>
2026-04-29 20:40:44 -0600

    NodeJS Course

---
+++ 4-names.js
@@ -0,0 +1,9 @@
+// Local
+const secret = "SUPER SECRET"
+// Share
+const john = 'john'
+const atom = 'Atom'
+
+module.exports = {john, atom}
+
+
---
+++ app.js
@@ -0,0 +1,7 @@
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+//console.log(names)
+
+sayHi(names.john)
+sayHi(names.atom)
+sayHi("Susan")
